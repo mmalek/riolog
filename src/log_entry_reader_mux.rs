7e3a580c95a72afac95ca5ac849f898f553e7bd7
Michał Małek <michalm@fastmail.fm>
2020-01-23 11:23:01 +0100

    Simplify LogEntryReaderMux implementation

--- src/log_entry_reader_mux.rs
+++ src/log_entry_reader_mux.rs
@@ -1,17 +1,16 @@
 use crate::log_entry::LogEntry;
-use std::cmp::Ordering;
 
 pub struct LogEntryReaderMux<I: Iterator<Item = LogEntry>> {
     input_iters: Vec<I>,
-    entries: Vec<(usize, Option<LogEntry>)>,
+    entries: Vec<LogEntry>,
 }
 
 impl<I: Iterator<Item = LogEntry>> LogEntryReaderMux<I> {
     pub fn new(mut input_iters: Vec<I>) -> Self {
         let entries = input_iters
             .iter_mut()
-            .map(|i| i.next())
             .enumerate()
+            .filter_map(|(index, iter)| iter.next().map(|entry| entry.with_source(index)))
             .collect();
         LogEntryReaderMux {
             input_iters,
@@ -24,24 +23,21 @@ impl<I: Iterator<Item = LogEntry>> Iterator for LogEntryReaderMux<I> {
     type Item = LogEntry;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.entries.retain(|(_, entry)| entry.is_some());
+        let item = self
+            .entries
+            .iter_mut()
+            .enumerate()
+            .min_by_key(|(_, e)| e.timestamp());
 
-        let item = self.entries.iter_mut().min_by(|(_, x), (_, y)| {
-            if let (Some(x), Some(y)) = (x, y) {
-                x.timestamp().cmp(&y.timestamp())
-            } else if x.is_some() {
-                Ordering::Less
-            } else if y.is_some() {
-                Ordering::Greater
+        if let Some((entries_index, log_entry)) = item {
+            let source = log_entry.source();
+            if let Some(mut entry) = self.input_iters[source].next() {
+                entry = entry.with_source(source);
+                std::mem::swap(log_entry, &mut entry);
+                Some(entry)
             } else {
-                Ordering::Equal
+                Some(self.entries.remove(entries_index))
             }
-        });
-
-        if let Some((i, log_entry)) = item {
-            let ret_log_entry = log_entry.take();
-            *log_entry = self.input_iters[*i].next();
-            ret_log_entry.map(|e| e.with_source(*i))
         } else {
             None
         }
@@ -68,23 +64,29 @@ mod tests {
         let mut reader = LogEntryReaderMux::new(iterators);
 
         assert_eq!(
-            reader.next().and_then(|e| e.timestamp()),
-            Some(NaiveDate::from_ymd(2020, 01, 01).and_hms(20, 0, 0))
+            reader.next().map(|e| (e.source(), e.timestamp())),
+            Some((0, Some(NaiveDate::from_ymd(2020, 01, 01).and_hms(20, 0, 0))))
         );
 
         assert_eq!(
-            reader.next().and_then(|e| e.timestamp()),
-            Some(NaiveDate::from_ymd(2020, 01, 01).and_hms(20, 30, 0))
+            reader.next().map(|e| (e.source(), e.timestamp())),
+            Some((
+                1,
+                Some(NaiveDate::from_ymd(2020, 01, 01).and_hms(20, 30, 0))
+            ))
         );
 
         assert_eq!(
-            reader.next().and_then(|e| e.timestamp()),
-            Some(NaiveDate::from_ymd(2020, 01, 01).and_hms(21, 0, 0))
+            reader.next().map(|e| (e.source(), e.timestamp())),
+            Some((0, Some(NaiveDate::from_ymd(2020, 01, 01).and_hms(21, 0, 0))))
         );
 
         assert_eq!(
-            reader.next().and_then(|e| e.timestamp()),
-            Some(NaiveDate::from_ymd(2020, 01, 01).and_hms(21, 30, 0))
+            reader.next().map(|e| (e.source(), e.timestamp())),
+            Some((
+                1,
+                Some(NaiveDate::from_ymd(2020, 01, 01).and_hms(21, 30, 0))
+            ))
         );
 
         assert_eq!(reader.next(), None);
