5813e11330dfcb9892e5c35517ef98501809ad71
Michał Małek <michalm@fastmail.fm>
2020-01-28 22:13:40 +0100

    Add unit tests for write_log() and write_log_fast() functions

--- src/main.rs
+++ src/main.rs
@@ -84,10 +84,10 @@ fn read_log(mut readers: Vec<impl BufRead>, writer: impl Write, opts: Options) -
 
         if entry_iters.len() == 1 {
             let entry_iter = entry_iters.pop().expect("No elements");
-            write_log(entry_iter, writer, opts.color_enabled, opts.input_files)
+            write_log(entry_iter, writer, opts.color_enabled, &opts.input_files)
         } else {
             let reader = LogEntryReaderMux::new(entry_iters);
-            write_log(reader, writer, opts.color_enabled, opts.input_files)
+            write_log(reader, writer, opts.color_enabled, &opts.input_files)
         }
     } else {
         let reader = readers.pop().expect("No elements");
@@ -135,7 +135,7 @@ fn write_log(
     log_entries: impl Iterator<Item = LogEntry>,
     mut writer: impl Write,
     color_enabled: bool,
-    input_files: Vec<PathBuf>,
+    input_files: &[PathBuf],
 ) -> Result<()> {
     for entry in log_entries {
         if input_files.len() > 1 {
@@ -180,6 +180,34 @@ fn write_log(
 #[cfg(test)]
 mod tests {
     use super::*;
+    use chrono::NaiveDate;
+
+    const LOREM_IPSUM: &[u8] = b"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed \
+do eiusmod tempor incididunt ut labore et dolore magna aliqua. In eu mi bibendum neque egestas \
+congue quisque egestas diam. Amet tellus cras adipiscing enim. Feugiat vivamus at augue eget \
+arcu dictum varius duis at. Integer enim neque volutpat ac tincidunt. Lobortis elementum nibh \
+tellus molestie. A iaculis at erat pellentesque. Porttitor rhoncus dolor purus non enim praesent \
+elementum facilisis. Sed blandit libero volutpat sed cras. Consectetur lorem donec massa sapien \
+faucibus et molestie.";
+
+    const ELEMENTUM_EU: &[u8] = b"Elementum eu facilisis sed odio morbi quis commodo odio \
+aenean. Quis eleifend quam adipiscing vitae. A iaculis at erat pellentesque adipiscing. Pulvinar \
+pellentesque habitant morbi tristique senectus et netus et malesuada. Commodo elit at imperdiet \
+dui. Volutpat est velit egestas dui id. Dictum sit amet justo donec enim diam vulputate ut. \
+Blandit libero volutpat sed cras ornare arcu dui vivamus arcu. At urna condimentum mattis \
+pellentesque id nibh tortor id aliquet. Quam pellentesque nec nam aliquam sem et tortor.";
+
+    const DT_FORMAT: &str = "%F %T.%3f";
+
+    fn header(year: i32, month: u32, day: u32, hour: u32, min: u32, sec: u32) -> Vec<u8> {
+        let buf = format!(
+            "-info:<22954> {} UTC [Category]: ",
+            NaiveDate::from_ymd(year, month, day)
+                .and_hms(hour, min, sec)
+                .format(DT_FORMAT)
+        );
+        buf.into_bytes()
+    }
 
     #[test]
     fn write_log_fast_simple_test() -> Result<()> {
@@ -189,6 +217,94 @@ mod tests {
         write_log_fast(&mut in_buf.as_slice(), &mut out_buf)?;
 
         assert_eq!(out_buf, b"abc\ndef\\ghi");
+
+        Ok(())
+    }
+
+    #[test]
+    fn write_log_fast_single_entry() -> Result<()> {
+        let mut in_buf = Vec::new();
+        in_buf.append(&mut header(2020, 01, 13, 20, 42, 00));
+        in_buf.append(&mut LOREM_IPSUM.to_vec());
+        in_buf.append(&mut b"\n\n".to_vec());
+
+        let mut out_buf = Vec::<u8>::new();
+
+        write_log_fast(&mut in_buf.as_slice(), &mut out_buf)?;
+
+        assert_eq!(out_buf, in_buf);
+
+        Ok(())
+    }
+
+    #[test]
+    fn write_log_fast_two_entries() -> Result<()> {
+        let mut in_buf = Vec::new();
+
+        in_buf.append(&mut header(2020, 01, 13, 20, 42, 00));
+        in_buf.append(&mut LOREM_IPSUM.to_vec());
+        in_buf.append(&mut b"\n\n".to_vec());
+
+        in_buf.append(&mut header(2020, 01, 13, 20, 43, 00));
+        in_buf.append(&mut ELEMENTUM_EU.to_vec());
+        in_buf.append(&mut b"\n\n".to_vec());
+
+        let mut out_buf = Vec::<u8>::new();
+
+        write_log_fast(&mut in_buf.as_slice(), &mut out_buf)?;
+
+        assert_eq!(out_buf, in_buf);
+
+        Ok(())
+    }
+
+    #[test]
+    fn write_log_single_entry_uncolored() -> Result<()> {
+        let mut contents = Vec::new();
+        contents.append(&mut header(2020, 01, 13, 20, 42, 00));
+        contents.append(&mut LOREM_IPSUM.to_vec());
+        contents.append(&mut b"\n\n".to_vec());
+
+        let entry = LogEntry::new(contents.clone());
+        let mut entries = vec![entry];
+        let input_files = [PathBuf::from("logname")];
+
+        let mut out_buf = Vec::<u8>::new();
+
+        write_log(entries.drain(..), &mut out_buf, false, &input_files)?;
+
+        let mut in_buf = Vec::new();
+        in_buf.append(&mut contents);
+        assert_eq!(out_buf, in_buf);
+
+        Ok(())
+    }
+
+    #[test]
+    fn write_log_two_entries_uncolored() -> Result<()> {
+        let mut contents1 = Vec::new();
+        contents1.append(&mut header(2020, 01, 13, 20, 42, 00));
+        contents1.append(&mut LOREM_IPSUM.to_vec());
+        contents1.append(&mut b"\n\n".to_vec());
+        let entry1 = LogEntry::new(contents1.clone());
+
+        let mut contents2 = Vec::new();
+        contents2.append(&mut header(2020, 01, 13, 20, 43, 00));
+        contents2.append(&mut ELEMENTUM_EU.to_vec());
+        contents2.append(&mut b"\n\n".to_vec());
+        let entry2 = LogEntry::new(contents2.clone());
+
+        let mut entries = vec![entry1, entry2];
+        let input_files = [PathBuf::from("logname")];
+
+        let mut out_buf = Vec::<u8>::new();
+
+        write_log(entries.drain(..), &mut out_buf, false, &input_files)?;
+
+        let mut in_buf = Vec::new();
+        in_buf.append(&mut contents1);
+        in_buf.append(&mut contents2);
+        assert_eq!(out_buf, in_buf);
 
         Ok(())
     }
